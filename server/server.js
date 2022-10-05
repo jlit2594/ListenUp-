c20cce4f042f813f4bf287b2ac070924ab19b189
jlit2594 <jdlitton2594@gmail.com>
2022-10-04 19:41:02 -0500

    set up the folder structure and installed dependencies

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+    // stuff
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const start ApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(ex[ress.static(path.join(__dirname, ''))])
+    }
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, ''))
+    })
+
+
+}
+
+startApolloServer();
