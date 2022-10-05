6c52591201c62ce58e1d36397d579e06ea11e7fb
jlit2594 <jdlitton2594@gmail.com>
2022-10-05 17:20:55 -0500

    wrote the majority of the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,65 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Search from './components/Search';
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import Profile from './pages/Profile';
+import SingleTrail from './pages/SingleTrail';
 
-import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        
+        <div className=''>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path="/"
+              element={<Home />}
+              />
+              <Route path="/login"
+              element={<Login />}
+              />
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route path="/trail/:id"
+              element={<SingleTrail />}
+              />
+
+              <Route path='*'
+              element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </ApolloProvider>
   );
