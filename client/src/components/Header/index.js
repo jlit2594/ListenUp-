d262df1d477714dff65ac72080a4d70cef320c33
jlit2594 <jdlitton2594@gmail.com>
2022-10-05 23:52:28 -0500

    added header element and installed bootstrap components

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Auth from '../../utils/auth';
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+
+        Auth.logout();
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>TITLE</h1>
+            </div>
+            <div>
+                {Auth.loggedIn() ? (
+                    <>
+                        <Button variant='danger'>Your Profile</Button>
+                        <Button variant='danger'>Logout</Button>
+                    </>
+                ) : (
+                    <>
+                        <Button variant='danger'>Login</Button>                     
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Header;
