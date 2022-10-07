13294fde4102a6f496095245735d4762fc521fe9
Inez Jimenez <ijim9182@yahoo.com>
2022-10-06 21:11:24 -0500

    password and user encryption

--- server/models/user.js
+++ server/models/user.js
@@ -30,7 +30,27 @@ userSchema.pre('save', function( next ) {
     }
 });
 
+userSchema.methods.generateTokens - function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secret')
 
+    user.token = token;
+    user.save(function (err, user) {
+        if(err) return cb(err)
+        cb(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secret', function(err, decode) {
+        user.findOne({"_id":decode, "token": token}, function(err, user) {
+            if(err) return cd(err);
+            cb(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User }
