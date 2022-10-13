3544f16f9e5347518435b9221ec256a0e3a123ef
J Litt <jdlitton2594@gmail.com>
2022-10-12 23:43:34 -0500

    trying to get the server to start

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/listen-up',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/listen-up',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
